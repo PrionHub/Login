fe49724d8eebc734b1158207eb8211123cabdb7b
Prion <personalcom969@gmail.com>
2026-07-09 17:05:11 -0500

    Update Components.lua

--- Modules/UI/Components.lua
+++ Modules/UI/Components.lua
@@ -253,32 +253,38 @@ function Components.CreateCheckBox(Properties)
     Label.Parent = Box
 
     local Enabled = false
-
+    
     Box.MouseButton1Click:Connect(function()
-
+    
         Enabled = not Enabled
-
+    
         Mark.Text = Enabled and "✓" or ""
-
+    
     end)
-
-    function Box:SetValue(Value)
-
-        Enabled = Value
-    
-        Mark.Text = Enabled and "✓" or ""
-    
-    end
-    
-    function Box:GetValue()
-    
-        return Enabled
-    
-    end
     
     ApplyCorner(Box)
     
-    return Box
+    return {
+    
+        Instance = Box,
+    
+        SetValue = function(Value)
+    
+            Enabled = Value
+    
+            Mark.Text = Enabled and "✓" or ""
+    
+        end,
+    
+        GetValue = function()
+    
+            return Enabled
+    
+        end
+    
+    }
+    
+    end
 
 end
 
