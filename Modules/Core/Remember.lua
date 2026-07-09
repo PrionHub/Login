1df3cff7456894b35c413bdb79c5dfac17c81e7f
Prion <personalcom969@gmail.com>
2026-07-09 16:12:40 -0500

    Implement Remember module for saving credentials

---
+++ Modules/Core/Remember.lua
@@ -0,0 +1,52 @@
+local Remember = {}
+
+local FILE_NAME = "prion_login.txt"
+
+function Remember.Save(username, password)
+
+    if not writefile then
+        return
+    end
+
+    local data = username .. "\n" .. password
+
+    writefile(FILE_NAME, data)
+
+end
+
+function Remember.Load()
+
+    if not (isfile and readfile) then
+        return nil
+    end
+
+    if not isfile(FILE_NAME) then
+        return nil
+    end
+
+    local content = readfile(FILE_NAME)
+
+    local username, password = content:match("([^\n]*)\n(.*)")
+
+    return {
+
+        Username = username,
+        Password = password
+
+    }
+
+end
+
+function Remember.Clear()
+
+    if delfile and isfile(FILE_NAME) then
+
+        if isfile(FILE_NAME) then
+            delfile(FILE_NAME)
+        end
+
+    end
+
+end
+
+return Remember
