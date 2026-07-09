afc570d6c659a9e1ebe77a80ca4ad0dec75fef64
Prion <personalcom969@gmail.com>
2026-07-09 12:29:20 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,24 +1,25 @@
 local Base =
-"https://raw.githubusercontent.com/PrionHub/Login/main/"
+    "https://raw.githubusercontent.com/PrionHub/Login/main/"
 
 local Theme =
-loadstring(game:HttpGet(Base.."Modules/UI/Theme.lua"))()
+    loadstring(game:HttpGet(Base.."Modules/UI/Theme.lua"))()
 
 local Components =
-loadstring(game:HttpGet(Base.."Modules/UI/Components.lua"))()
+    loadstring(game:HttpGet(Base.."Modules/UI/Components.lua"))()
 
 local Build =
     loadstring(game:HttpGet(Base.."Modules/UI/Build.lua"))()
 
-App.Build = Build
-
-Build.Init(App)
-
-local UI = Build.Build()
-
+-- Crear App primero
 local App = {}
 
 App.Theme = Theme
 App.Components = Components
+App.Build = Build
 
+-- Inicializar módulos
 Components.Init(App)
+Build.Init(App)
+
+-- Construir la interfaz
+local UI = Build.Build()
