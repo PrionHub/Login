f82d84daf8ae9277dfa985dbf868cb7b4d181ecb
Prion <personalcom969@gmail.com>
2026-07-09 11:52:09 -0500

    Load theme and components from external scripts

--- Main.lua
+++ Main.lua
@@ -1,1 +1,10 @@
---
+local Base =
+"https://raw.githubusercontent.com/PrionHub/Login/main/"
+
+local Theme =
+loadstring(game:HttpGet(Base.."Modules/UI/Theme.lua"))()
+
+local Components =
+loadstring(game:HttpGet(Base.."Modules/UI/Components.lua"))()
+
+Components.Init(Theme)
