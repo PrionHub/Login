1b879dd13f712c9c80f86f18a990f8bbec30fb60
Prion <personalcom969@gmail.com>
2026-07-09 12:27:45 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -7,6 +7,15 @@ loadstring(game:HttpGet(Base.."Modules/UI/Theme.lua"))()
 local Components =
 loadstring(game:HttpGet(Base.."Modules/UI/Components.lua"))()
 
+local Build =
+    loadstring(game:HttpGet(Base.."Modules/UI/Build.lua"))()
+
+App.Build = Build
+
+Build.Init(App)
+
+local UI = Build.Build()
+
 local App = {}
 
 App.Theme = Theme
