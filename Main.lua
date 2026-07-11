bffe5bd996adb7a5eaa06a95e9c7b9be946ca605
Prion <personalcom969@gmail.com>
2026-07-10 19:38:37 -0500

    Enhance registration/login UI with animations
    Refactor UI behavior for registration and login modes, including animations.

--- Main.lua
+++ Main.lua
@@ -1,3 +1,4 @@
+local TweenService = game:GetService("TweenService")
 local Base =
     "https://raw.githubusercontent.com/PrionHub/Login/main/"
 
@@ -38,35 +39,19 @@ local UI = Build.Build()
 
 local RegisterMode = false
 
-UI.SwitchMode.MouseButton1Click:Connect(function()
-
-    RegisterMode = not RegisterMode
-
-    UI.KeyBox.Visible = RegisterMode
-
-    if RegisterMode then
-
-        UI.LoginButton.Text = "Register"
-
-        UI.SwitchMode.Text = "Already have an account? Login"
-
-    else
-
-        UI.LoginButton.Text = "Login"
-
-        UI.SwitchMode.Text = "Don't have an account? Register"
-
-    end
-
-end)
-
 Particles.Create(UI.MainFrame, UI.ParticleContainer)
 
 UI.SwitchMode.MouseButton1Click:Connect(function()
 
-    if UI.Mode == "Login" then
+    RegisterMode = not RegisterMode
 
-        UI.Mode = "Register"
+    local NewSize
+    local RememberPos
+    local LoginPos
+    local GetKeyPos
+    local SwitchPos
+
+    if RegisterMode then
 
         UI.KeyBox.Visible = true
 
@@ -75,16 +60,82 @@ UI.SwitchMode.MouseButton1Click:Connect(function()
         UI.SwitchMode.Text =
             "Already have an account? Login"
 
+        NewSize = UDim2.fromOffset(280, 380)
+
+        RememberPos = UDim2.fromOffset(30, 242)
+
+        LoginPos = UDim2.fromOffset(20, 275)
+
+        GetKeyPos = UDim2.fromOffset(20, 320)
+
+        SwitchPos = UDim2.fromOffset(30, 355)
+
     else
-
-        UI.Mode = "Login"
-
-        UI.KeyBox.Visible = false
 
         UI.LoginButton.Text = "Login"
 
         UI.SwitchMode.Text =
             "Don't have an account? Register"
+
+        NewSize = UDim2.fromOffset(280, 330)
+
+        RememberPos = UDim2.fromOffset(30, 200)
+
+        LoginPos = UDim2.fromOffset(20, 230)
+
+        GetKeyPos = UDim2.fromOffset(20, 275)
+
+        SwitchPos = UDim2.fromOffset(30, 310)
+
+    end
+
+    local TweenInfo = TweenInfo.new(
+
+        0.35,
+
+        Enum.EasingStyle.Quint,
+
+        Enum.EasingDirection.Out
+
+    )
+
+    TweenService:Create(
+        UI.MainFrame,
+        TweenInfo,
+        {Size = NewSize}
+    ):Play()
+
+    TweenService:Create(
+        UI.Remember.Instance,
+        TweenInfo,
+        {Position = RememberPos}
+    ):Play()
+
+    TweenService:Create(
+        UI.LoginButton,
+        TweenInfo,
+        {Position = LoginPos}
+    ):Play()
+
+    TweenService:Create(
+        UI.GetKeyButton,
+        TweenInfo,
+        {Position = GetKeyPos}
+    ):Play()
+
+    TweenService:Create(
+        UI.SwitchMode,
+        TweenInfo,
+        {Position = SwitchPos}
+    ):Play()
+
+    if not RegisterMode then
+
+        task.delay(0.35, function()
+
+            UI.KeyBox.Visible = false
+
+        end)
 
     end
 
