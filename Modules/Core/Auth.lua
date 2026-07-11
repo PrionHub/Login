57524424f1a2f0ad689106dfd16fe71edc6634a7
Prion <personalcom969@gmail.com>
2026-07-11 08:23:27 -0500

    Create Auth.lua

---
+++ Modules/Core/Auth.lua
@@ -0,0 +1,15 @@
+local Auth = {}
+
+local HttpService = game:GetService("HttpService")
+
+local URL_API = "https://keyauth.win/api/1.3/"
+
+local KeyAuth_Name = "PrionHub"
+local KeyAuth_Owner = "kYbqiy1KMp"
+local KeyAuth_Version = "1.0"
+
+function Auth.Init()
+
+end
+
+return Auth
