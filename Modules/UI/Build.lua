4dc4546a378e02f0b00b207d8fa1213d3359ebf8
Prion <personalcom969@gmail.com>
2026-07-11 07:56:39 -0500

    Adjust MainFrame height in Build.lua

--- Modules/UI/Build.lua
+++ Modules/UI/Build.lua
@@ -44,7 +44,7 @@ local function CreateMainFrame(ScreenGui)
 
         Parent = ScreenGui,
 
-        Size = UDim2.fromOffset(280, 390),
+        Size = UDim2.fromOffset(280, 340),
 
         Position = UDim2.fromScale(0.5, 0.5),
 
