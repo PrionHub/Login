1f77fe2e2a275ce53dbf0f179a832bbb324cc851
Prion <personalcom969@gmail.com>
2026-07-11 09:01:09 -0500

    Update Auth.lua

--- Modules/Core/Auth.lua
+++ Modules/Core/Auth.lua
@@ -2,52 +2,62 @@ local Auth = {}
 
 local HttpService = game:GetService("HttpService")
 
-local App
-
 local Name = "PrionHub"
-local OwnerId = "kYbqiy1KMp"
+local OwnerId = "kYbqjy1KMp"
+local Secret = "a97af28167838caa7db505e0f245ae989d47ac1d0d60e70fb76eb7e9793286ef"
 local Version = "1.0"
 
-local SessionId = nil
+local SessionId
 
-local function Init()
+local httpRequest =
+    syn and syn.request
+    or http_request
+    or request
 
-    local response = game:HttpGet(
+function Auth.Init()
 
-        "https://keyauth.win/api/1.1/"
-        .. "?name=" .. Name
-        .. "&ownerid=" .. OwnerId
-        .. "&type=init"
-        .. "&ver=" .. Version
+    local response = httpRequest({
 
+        Url = "https://keyauth.win/api/1.2/",
+
+        Method = "POST",
+
+        Headers = {
+
+            ["Content-Type"] =
+                "application/x-www-form-urlencoded"
+
+        },
+
+        Body = string.format(
+
+            "type=init&name=%s&ownerid=%s&secret=%s&version=%s",
+
+            Name,
+            OwnerId,
+            Secret,
+            Version
+
+        )
+
+    })
+
+    local data = HttpService:JSONDecode(
+        response.Body
     )
-
-    print("RESPUESTA:")
-    print(response)
-
-    local data = HttpService:JSONDecode(response)
 
     if data.success then
 
         SessionId = data.sessionid
 
-        print("Sesion iniciada:", SessionId)
+        print("Sesión iniciada:", SessionId)
 
         return true
-
     end
 
-    warn("KeyAuth error:", data.message)
+    warn(data.message)
 
     return false
-
-end
-function Auth.Init(Application)
-
-    App = Application
-
-    Init()
-
 end
 
 return Auth
