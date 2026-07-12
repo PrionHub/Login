b95d715a262592bbac63fdb1ff3379cdcaf1f714
Prion <personalcom969@gmail.com>
2026-07-11 19:52:37 -0500

    Implement drag functionality for UI frames

---
+++ Modules/UI/Drag.lua
@@ -0,0 +1,62 @@
+local Drag = {}
+
+local UserInputService =
+    game:GetService("UserInputService")
+
+function Drag.Enable(Frame)
+
+    local Dragging = false
+    local DragStart
+    local StartPosition
+
+    local function Update(Input)
+
+        local Delta = Input.Position - DragStart
+
+        Frame.Position = UDim2.new(
+
+            StartPosition.X.Scale,
+            StartPosition.X.Offset + Delta.X,
+
+            StartPosition.Y.Scale,
+            StartPosition.Y.Offset + Delta.Y
+
+        )
+
+    end
+
+    Frame.InputBegan:Connect(function(Input)
+
+        if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+
+            Dragging = true
+            DragStart = Input.Position
+            StartPosition = Frame.Position
+
+            Input.Changed:Connect(function()
+
+                if Input.UserInputState == Enum.UserInputState.End then
+
+                    Dragging = false
+
+                end
+
+            end)
+
+        end
+
+    end)
+
+    UserInputService.InputChanged:Connect(function(Input)
+
+        if Dragging and Input.UserInputType == Enum.UserInputType.MouseMovement then
+
+            Update(Input)
+
+        end
+
+    end)
+
+end
+
+return Drag
